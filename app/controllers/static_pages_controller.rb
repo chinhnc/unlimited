e17a8f61bc5597beead5c1e14fff0eafa1f25150
chinhnc <chinhharub@gmail.com>
2016-04-10 22:52:07 +0700

    add image posts and layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @images = Image.all
+  end
+
+  def start
+    if user_signed_in?
+      redirect_to home_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  root to: "static_pages#home"
+  root to: "static_pages#start"
+  get 'home' => 'static_pages#home'
 end
