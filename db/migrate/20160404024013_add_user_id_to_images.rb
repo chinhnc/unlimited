752ecef11a7c2a10bbcc226f256dc864349e41cc
chinhnc <chinhharub@gmail.com>
2016-04-23 18:19:50 +0700

    fix bug deploy 3

--- db/migrate/20160404024013_add_user_id_to_images.rb
+++ db/migrate/20160404024013_add_user_id_to_images.rb
@@ -1,5 +1,6 @@
 class AddUserIdToImages < ActiveRecord::Migration
   def change
+    add_column :images, :user_id, :integer
     add_reference :images, :users, index: true, foreign_key: true
   end
 end
