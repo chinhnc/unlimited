fd6e6cd675a3805e5b3e6ed90832f8e9e42f4264
chinhnc <chinhharub@gmail.com>
2016-04-24 21:43:34 +0700

    fix bug css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def current_user?(user)
-    user == current_user
+  def full_title(page_title = '')
+    base_title = "Girl Photo"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title}"
+    end
   end
 end
